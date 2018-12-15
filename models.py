8f3c4f8a8cccc51053b86f0bad2e2931147ca899
malinroot <kurchevskijpavel@gmail.com>
2018-12-15 22:07:11 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+SHORT_TEXT_LEN = 1000
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
+
+
+    def get_short_text(self):
+        if len(self.text) > SHORT_TEXT_LEN:
+            return self.text[:SHORT_TEXT_LEN]
+        else:
+            return self.text
